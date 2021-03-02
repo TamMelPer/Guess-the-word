311a7a083b0b84c886ad9ff3f63de2de85a86604
Melissa <mystic101@gmail.com>
2021-03-02 11:12:23 +0000

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react'
+import Enzyme from 'enzyme'
 import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 test('renders learn react link', () => {
   render(<App />);
