98d3327888db4db806fb1037a67ec24d8ccc0ce5
Lucy Stringer <lucyjstringer@gmail.com>
2021-03-05 11:49:47 +0000

    added passing test for guessed words rendering without error when no words guessed

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 const GuessedWords = (props) => {
   return (
-    <div />
+    <div data-test="component-guessed-words">
+
+    </div>
   );
 };
 
--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -23,3 +23,18 @@ const setup = (props={}) => {
 test('it does not throw warning with expected props', () => {
   checkProps(GuessedWords, defaultProps);
 });
+
+describe('if there are no words guessed', () => {
+  test('renders without error', () => {
+    const wrapper = setup({ guessedWords: [] });
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+  test('renders instructions to guess a word', () => {
+
+  });
+});
+
+describe('if there are words guessed', () => {
+
+});
