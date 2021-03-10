4704ca204e65ffc6876776337873b892bdff1a94
Melissa <mystic101@gmail.com>
2021-03-10 11:22:45 +0000

    added storeFactory function to testUtils

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,18 @@
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
+
+import rootReducer from '../src/reducers';
+
+/**
+ * Create a testing store with imported reducers, middleware, and initial state.
+ * globals: rootReducer.
+ * @param {object} initialState  - Initial state for store
+ * @function storeFactory
+ * @returns {Store} - Redux store
+ */
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+}
 
 /**
  * Return node(s) with the given data-test attribute.
@@ -6,7 +20,6 @@ import checkPropTypes from 'check-prop-types';
  * @param {string} val - Value of the data-test attribute for search.
  * @returns {ShallowWrapper} 
  */
-
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
 } 
