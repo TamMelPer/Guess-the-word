705268dae3758ed67ead7d551a00153c10ec126b
Melissa <mystic101@gmail.com>
2021-03-10 10:35:16 +0000

    passing reducer test that returns state of true upon receiving an action of type CORRECT_GUESS

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,4 @@
+import { actionTypes } from '../actions';
 /**
  * @function successReducer
  * @param {array} state - Array of guessed words
@@ -6,6 +7,11 @@
  */
 
 
-export default (state, action) => {
-    return false;
+export default (state=false, action) => {
+  switch(action.type) {
+    case (actionTypes.CORRECT_GUESS):
+      return true;
+    default:
+      return state;
+  }
 }
