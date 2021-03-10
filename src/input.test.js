7cde0d924fd35c3c7818a6e51d140f9d3294c933
Melissa <mystic101@gmail.com>
2021-03-10 11:06:04 +0000

    added boiler plate tests for Input component

--- src/input.test.js
+++ src/input.test.js
@@ -4,4 +4,31 @@ import { shallow } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 import Input from './Input';
 
+describe('render', () => {
+  describe('word has not been guessed', () => {
+    test('renders component without error', () => {
 
+    });
+    test('renders input box', () => {
+
+    });
+    test('renders submit button', () => {
+
+    });
+  });
+  describe('word has been guessed', () => {
+    test('renders component without error', () => {
+
+    });
+    test('does not render input box', () => {
+
+    });
+    test('does not render submit button', () => {
+
+    });
+  });
+});
+
+describe('update state', () => {
+
+});
